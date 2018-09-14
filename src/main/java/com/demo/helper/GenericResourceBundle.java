3a1136e67e01d1b70d37c2d342cbc719daa9cb46
Manika Bedi <manikabedi@yahoo.com>
2018-09-14 14:37:09 +0530

    Changes to path

---
+++ src/main/java/com/demo/helper/GenericResourceBundle.java
@@ -0,0 +1,24 @@
+package com.demo.helper;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+public class GenericResourceBundle {
+	public static String getProperties(String source){
+		ResourceBundle rb = ResourceBundle.getBundle("ResourceBundle");
+		Enumeration <String> keys = rb.getKeys();
+		String value="";
+		while (keys.hasMoreElements()) {
+			
+			String key =  keys.nextElement();
+			
+			if(key.equalsIgnoreCase(source)){
+				value = rb.getString(key);
+			}
+		// just some comments
+		}
+		return value;
+	}
+	
+}
+
